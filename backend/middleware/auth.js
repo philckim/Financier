d6de4ccac2a2a6f468e99ca9cef6984f69d6551b
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2021-04-09 17:14:59 -0700

    Update backend/middleware/auth.js
    Co-authored-by: Jeremiah <42397676+jlsnow301@users.noreply.github.com>

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,6 @@ const config = require('config');
 module.exports = function(req, res, next) {
     const token = req.header('x-auth-token');
 
-    // Check if no token
     if(!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
     }
