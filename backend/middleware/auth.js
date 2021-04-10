0b39add7b3e7eb54a8117c2cd6515d70a8ed5cdc
All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>
2021-04-09 17:14:54 -0700

    Update backend/middleware/auth.js
    Co-authored-by: Jeremiah <42397676+jlsnow301@users.noreply.github.com>

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function(req, res, next) {
-    // Get token from header
     const token = req.header('x-auth-token');
 
     // Check if no token
