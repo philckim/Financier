3bf5e5a884a1facf80b4977ecd5964bb1ff5e5a0
All-Schuck-Up <schuckrw@gmail.com>
2021-04-15 10:05:27 -0700

    Update plaid.js

--- backend/routes/api/plaid.js
+++ backend/routes/api/plaid.js
@@ -20,6 +20,7 @@ const client = new plaid.Client({
 // @access  Public
 router.get('/create-link-token', auth, async (req, res) => {
     const user = await User.findById(req.user.id);
+
     try {
       // console.log(user);
       // console.log(`backend create-link-token req by: ${user.id}`);
@@ -34,7 +35,7 @@ router.get('/create-link-token', auth, async (req, res) => {
       });
 
       res.json({ linkToken });
-      console.log('create-link-token success! token: ', {linkToken})
+      // console.log('create-link-token success! token: ', {linkToken})
 
     } catch (err) {
         return res.send({ err: err.message })
@@ -46,27 +47,54 @@ router.get('/create-link-token', auth, async (req, res) => {
 // @access  Public
 router.post('/token-exchange', auth, async (req, res) => {
   const user = await User.findById(req.user.id);
-
-    // console.log(req.body);
-    const { publicToken } = req.body.publicToken;
+  console.log(req.body.metadata);
+  const institution = req.body.metadata.institution;
+  const { name, institution_id } = institution;
 
     try {
-      console.log('backend token exchange...')
+      console.log('Attempting backend plaid token exchange...')
       const { publicToken } = req.body;
-      const { access_token: accessToken } = await client.exchangePublicToken(publicToken);
+      const { access_token: accessToken, item_id: itemId } = await client.exchangePublicToken(publicToken);
 
+      // onSuccess -> save access_token for future use
+      if(accessToken) {
+        // Check if account exists
+        // console.log(`accessToken: ${accessToken}, itemId: ${itemId}`);
+        let account = await Account.findOne({
+          userId: user.id,
+          institutionId: institution_id
+        })
+        if(account) {
+          console.log(`Account already linked!`);
+        } else {
+          // Create account and save to DB
+          console.log('Account not found, Creating account...')
+          account = new Account({
+            userId: user.id,
+            accessToken: accessToken,
+            itemId: itemId,
+            institutionId: institution_id,
+            institutionName: name
+          });
+
+          account.save();
+          // console.log(`Account created:  ${account}`);
+          res.json(account)
+        }
+      }
+      
       //fetch account data from plaid api
-      const balanceResponse = await client.getBalance(accessToken);
-      const transactionResponse = await client.getTransactions(
-        accessToken,
-        moment().subtract("3", "months").format("YYYY-MM-DD"),
-        moment().format("YYYY-MM-DD"),
-        { count: 300, offset: 0}
-      );
+      // const balanceResponse = await client.getBalance(accessToken);
+      // const transactionResponse = await client.getTransactions(
+      //   accessToken,
+      //   moment().subtract("3", "months").format("YYYY-MM-DD"),
+      //   moment().format("YYYY-MM-DD"),
+      //   { count: 300, offset: 0}
+      // );
 
       // console.log('Account Balance: ', balanceResponse);
       // console.log('Transaction Data: ', transactionResponse);
-      res.json({ balanceResponse, transactionResponse });
+      // res.json({ balanceResponse, transactionResponse });
       console.log('token exchange success!')
    } catch (err) {
       return res.send({ err: err.message })
@@ -76,12 +104,18 @@ router.post('/token-exchange', auth, async (req, res) => {
 // @route GET api/plaid/accounts
 // @desc Get all accounts linked with plaid for a specific user
 // @access Private
-router.get('/accounts', auth, (req, res) => {
-    Account.find({ userId: req.user.id })
-      .then(accounts => res.json(accounts))
-      .catch(err => console.log(err));
+router.get('/accounts', auth, async (req, res) => {
+  try {
+    const accounts = await Account.find({ userId: req.user.id });
+
+    if(!accounts) {
+      console.log(`No accounts found for user: ${req.user.name}`);
+    }
+    accounts.map(account => res.json(account));
+  } catch (err) {
+    return res.send({ err: err.message })
   }
-);
+});
 
 // @route POST api/plaid/accounts
 // @desc Get all accounts linked with plaid for a specific user
