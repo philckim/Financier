72050903e2d20e2f2f48bc8fac932ca4384f214f
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2021-04-23 16:11:54 -0700

    By Suggestion
    Co-Authored-By: All-Schuck-Up <55608123+All-Schuck-Up@users.noreply.github.com>

--- backend/routes/api/plaid.js
+++ backend/routes/api/plaid.js
@@ -56,6 +56,69 @@ router.get("/create-link-token", auth, async (req, res, next) => {
 });
 
 /**
+ *  @route   POST api/plaid
+ *  @desc    Plaid token exchange:
+ *  @params  {body: publicToken, metadata, auth.token, headers: 'x-auth-token'}
+ *  @access  Private
+ */
+router.post("/token-exchange", auth, async (req, res, next) => {
+  /** Send token through auth middleware -> decode token and asign userId to user */
+  const user = await User.findById(req.user.userId);
+
+  /** Pull linked bank acount info from meta data */
+  const institution = req.body.metadata.institution;
+  const { name, institution_id } = institution;
+
+  const { publicToken } = req.body;
+
+  let accessToken, itemId;
+  try {
+    const { access_token, item_id } = await client.exchangePublicToken(
+      publicToken
+    );
+    accessToken = access_token;
+    itemId = item_id;
+  } catch (err) {
+    const error = new HttpError("Could not retrieve access token.", 500);
+    return next(error);
+  }
+
+  /** Check if account already exists */
+  let account;
+  try {
+    account = await Account.findOne({
+      userId: user.id,
+      institutionId: institution_id,
+    });
+  } catch (err) {
+    const error = new HttpError("Could not fetch account.", 500);
+    return next(error);
+  }
+
+  if (account) {
+    const error = new HttpError("Account already linked.", 405);
+    return next(error);
+  }
+
+  /** Create account and save to DB  */
+  try {
+    account = new Account({
+      userId: user.id,
+      accessToken: accessToken,
+      itemId: itemId,
+      institutionId: institution_id,
+      institutionName: name,
+    });
+    account.save();
+  } catch (err) {
+    const error = new HttpError("Could not create account.", 500);
+    return next(error);
+  }
+
+  res.json(account);
+});
+
+/**
  *  @route  GET api/plaid/accounts
  *  @desc   Get all accounts linked with plaid for a specific user
  *  @access Private
@@ -129,69 +192,6 @@ router.get("/accounts/:accountId", auth, async (req, res, next) => {
   }
 
   res.json({ balanceResponse, transactionResponse });
-});
-
-/**
- *  @route   POST api/plaid
- *  @desc    Plaid token exchange:
- *  @params  {body: publicToken, metadata, auth.token, headers: 'x-auth-token'}
- *  @access  Private
- */
-router.post("/token-exchange", auth, async (req, res, next) => {
-  /** Send token through auth middleware -> decode token and asign userId to user */
-  const user = await User.findById(req.user.userId);
-
-  /** Pull linked bank acount info from meta data */
-  const institution = req.body.metadata.institution;
-  const { name, institution_id } = institution;
-
-  const { publicToken } = req.body;
-
-  let accessToken, itemId;
-  try {
-    const { access_token, item_id } = await client.exchangePublicToken(
-      publicToken
-    );
-    accessToken = access_token;
-    itemId = item_id;
-  } catch (err) {
-    const error = new HttpError("Could not retrieve access token.", 500);
-    return next(error);
-  }
-
-  /** Check if account already exists */
-  let account;
-  try {
-    account = await Account.findOne({
-      userId: user.id,
-      institutionId: institution_id,
-    });
-  } catch (err) {
-    const error = new HttpError("Could not fetch account.", 500);
-    return next(error);
-  }
-
-  if (account) {
-    const error = new HttpError("Account already linked.", 405);
-    return next(error);
-  }
-
-  /** Create account and save to DB  */
-  try {
-    account = new Account({
-      userId: user.id,
-      accessToken: accessToken,
-      itemId: itemId,
-      institutionId: institution_id,
-      institutionName: name,
-    });
-    account.save();
-  } catch (err) {
-    const error = new HttpError("Could not create account.", 500);
-    return next(error);
-  }
-
-  res.json(account);
 });
 
 /**
