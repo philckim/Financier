d7f5e880dc0f2abfef9926b4aae845500bc61634
All-Schuck-Up <schuckrw@gmail.com>
2021-06-15 16:11:57 -0700

    prep for deployment

--- server.js
+++ server.js
@@ -51,6 +51,16 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured." });
 });
 
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client-web/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-web', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
